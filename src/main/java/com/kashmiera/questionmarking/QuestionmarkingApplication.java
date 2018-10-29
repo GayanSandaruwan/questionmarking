5238fd371815d96d62e725ab0a77799d53a37d01
gayansandaruwan <gayankavirathne.14@cse.mrt.ac.lk>
2018-10-29 16:33:32 +0530

    Initial Controller Tested for hello

---
+++ src/main/java/com/kashmiera/questionmarking/QuestionmarkingApplication.java
@@ -0,0 +1,12 @@
+package com.kashmiera.questionmarking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionmarkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestionmarkingApplication.class, args);
+    }
+}
