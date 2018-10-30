12772c3695ac92b93a0c3860b10f4718acf62619
gayansandaruwan <gayankavirathne.14@cse.mrt.ac.lk>
2018-10-30 13:00:39 +0530

    Installing non maven central dependencies

---
+++ src/main/resources/lib/install-EXT-LIBS.bat
@@ -0,0 +1,3 @@
+call mvn install:install-file -Dfile=jawjaw-1.0.2/jawjaw-1.0.2.jar -DgroupId=edu.cmu.lti -DartifactId=jawjaw -Dversion=1.0.2 -Dpackaging=jar
+call mvn install:install-file -Dfile=ws4j-1.0.1/ws4j-1.0.1.jar -DgroupId=edu.cmu.lti -DartifactId=ws4j -Dversion=1.0.1 -Dpackaging=jar
+call mvn install:install-file -Dfile=libsvm.jar -DgroupId=com.fasterxml.jackson.core -DartifactId=jackson-databind -Dversion=2.9.5 -Dpackaging=jar
